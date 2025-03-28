d461915d6f752479f956abdb1be679be0cd385ab
SorrenJ <soalexjao@gmail.com>
2025-03-28 09:45:46 -0700

    change route

--- reciever_test.js
+++ reciever_test.js
@@ -4,14 +4,14 @@ const axios = require("axios");
 const app = express();
 
 // Function to fetch and log EC2 response periodically
-// const fetchAndLogEC2Data = async () => {
-//     try {
-//         const response = await axios.get("http://13.59.152.110:8080/");
-//         console.log("Periodic EC2 Response:", response.data);
-//     } catch (error) {
-//         console.error("Error fetching EC2 data:", error.message);
-//     }
-// };
+const fetchAndLogEC2Data = async () => {
+    try {
+        const response = await axios.get("http://13.59.152.110:8080/");
+        console.log("Periodic EC2 Response:", response.data);
+    } catch (error) {
+        console.error("Error fetching EC2 data:", error.message);
+    }
+};
 
 // Fetch EC2 data every 10 seconds
 // setInterval(fetchAndLogEC2Data, 10000);
@@ -33,5 +33,5 @@ app.listen(PORT, () => {
     console.log(`Local server running on port ${PORT}`);
 
     // Fetch data once on startup
-    // fetchAndLogEC2Data();
+    fetchAndLogEC2Data();
 });
